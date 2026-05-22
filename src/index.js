404a53d0062711aa27c95e41f1b590648754f70f
a.savanovich <asavanster@gmail.com>
2026-05-22 12:08:59 +0300

    reconnect version

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import install from "./js/utils.js";
 if (__USE_SERVICE_WORKERS__) {
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.register("./sw.js", {scope: "./"});
-        install(window, document);
     }
 }
 
+install(window, document);
 starter(window, document);
